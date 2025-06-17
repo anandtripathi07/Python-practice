5867464e38aab9a0a0e373fa00eeb2fc4f87c38e
Anandtripathi07 <tripa.anand1423@gmail.com>
2025-06-17 10:11:16 +0530

    python practice

---
+++ python.py
@@ -0,0 +1,56 @@
+# # '''print("hello world")
+# # name="Anand"
+# # print(name)
+# # print(name[2:4])
+# # text = "Python is a easy and simple programming language"
+# # t1 = text[0:7]
+# # t2 = text[7:49]
+# # t3 = t1+t2
+# # print(t3)
+# # #Python is a easy and simple programming language
+# # multi_line =
+# # am
+# # Anand
+# # print(multi_line)
+# # n1 = "Anand tripathi"
+# # n2 = "Aarush kadam"
+# # r1 = 50
+# # r2 = 7
+# # n3 = n1 +str(r1)
+# # n4 = n2 +str(r2)
+# # print(n3)
+# # print(n4)
+# # name = input("What is ur name?")
+# # print("Name:-", name)
+# # x1 = input("My marks:-")
+# # x2 = input("Teammate marks:-")
+# # if(x1>x2):
+# #     print("Anand have greater marks than his teammate")
+# # elif(x1<x2):
+# #     print("Teammate has greater marks than Anand")
+# #     '''
+
+# rn = [10,20,30,40,50,60]
+# # # rn.append(70)
+# # # rn.remove(10)
+# # # rn.del(1)
+# # del rn[1]
+# # print(rn)
+# # rn.sort()
+# # print(rn)
+# # rn.reverse()
+# # print(rn)
+# # print(rn.count(10))
+# y = {
+#     "name": "Anand",
+#     "Roll": "50"
+# }
+# print(y)
+
+
+def add():
+    num1 = input("Enter first number:")
+    num2 = input("Enter second number:")
+    sum = int(num1)+int(num2)
+    print("The sum is:",sum)
+add()
